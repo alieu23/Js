a4b61e23f93389b1ce34472ab265bce7bdbb1725
alieu23 <alieusamateh86@gmail.com>
2021-06-02 17:04:53 +0000

    Addede data title component

--- covid-tracker/src/views/Home.vue
+++ covid-tracker/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <main v-if=!loading>
-    Show Data
+    <DataTitle :text="title" :dataDate="dataDate" />
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -11,8 +11,7 @@
 </template>
 
 <script>
-// @ is an alias to /src
-
+import DataTitle from '../components/DataTitle'
 export default {
   name: 'Home',
   data(){
@@ -28,7 +27,7 @@ export default {
 
   },
   components: {
-
+        DataTitle
   },
   methods:{
     async fetchCovidData(){
@@ -45,7 +44,7 @@ export default {
     this.dataDate= data.Date
     this.stats= data.Global
     this.countries= data.Countries
-    this.loading= true
+    this.loading= false
 
 
   }
