ba2b7e60879e07f513637901f8920aa9e16da0c4
alieu23 <alieusamateh86@gmail.com>
2021-06-08 17:11:58 +0000

    Data boxes added

---
+++ covid-tracker/src/components/CountrySelect.vue
@@ -0,0 +1,28 @@
+<template>
+   <Select @change="onChange()"
+     class="form-select mt-1 block w-full border p-3 rounded"
+     v-model="selected">
+       <option value="select country">Select Country</option>
+       <option v-for="(country, index) in countries" :key="index" :value="country.ID">
+           {{country.Country}}
+       </option>
+   </Select>
+</template>
+<script>
+export default {
+    name:'CountrySelect',
+    props:['countries'],
+    data(){
+        return{
+            selected:0,
+        }
+    },
+    methods:{
+        onChange(){
+            const country = this.countries.find((item)=> item.ID === "Gambia")
+            console.log('selected', country)
+            this.$emit('get-country', country)
+        },
+    },
+}
+</script>
--- covid-tracker/src/views/Home.vue
+++ covid-tracker/src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <main v-if=!loading>
     <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+    
+    <button @click="clearCountryData"
+    v-if="stats.Country"
+     class="bg-green-700 text-white rounded p-3 mt-10
+     focus:outline-none hover:bg-green-600">
+     Clear Country
+     </button>
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">
@@ -12,6 +21,8 @@
 
 <script>
 import DataTitle from '../components/DataTitle'
+import DataBoxes from '../components/DataBoxes'
+import CountrySelect from '../components/CountrySelect'
 export default {
   name: 'Home',
   data(){
@@ -27,15 +38,26 @@ export default {
 
   },
   components: {
-        DataTitle
+        DataTitle,
+        DataBoxes,
+        CountrySelect
   },
   methods:{
     async fetchCovidData(){
       const res = await fetch('https://api.covid19api.com/summary')
       const data = await res.json()
       return data
-
-
+    },
+    getCountryData(country){
+      this.stats = country
+      this.title = country.Country
+    },
+    async clearCountryData(){
+      this.loading= true
+      const data = await this.fetchCovidData()
+      this.title="Global"
+      this.stats = data.Global
+      this.loading=false
     }
   },
   async created(){
@@ -45,7 +67,6 @@ export default {
     this.stats= data.Global
     this.countries= data.Countries
     this.loading= false
-
 
   }
 }
